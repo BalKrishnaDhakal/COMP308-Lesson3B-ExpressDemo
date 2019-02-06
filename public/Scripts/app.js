bc61b633f489f766d141f0f6e30bb19e23c0dc9f
Bal krishna Dhakal <bdhakal1@my.centennialcollege.ca>
2019-02-06 17:26:05 -0500

    added bootstrap

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,1 +1,9 @@
 /*  Custome Javascript goes here */
+//IIFF -- Immediately Invoked Function Expression
+(function() {
+    function Start() {
+        console.log(`%c App Started...`, "font-size:20px; color:blue;font-weight:bold");
+
+    }
+    window.addEventListener("load", Start);
+})();
